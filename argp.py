12e53ae0d759f5b3b8f5547966dd2fe8e8719b30
l1nux-th1ngz <137152321+l1nux-th1ngz@users.noreply.github.com>
2023-11-13 07:34:36 -0700

    argp.py

--- argp.py
+++ argp.py
@@ -7,13 +7,14 @@ pkgdesc="GUI Chat App with GPT for Arch Linux"
 arch=('any')
 url="https://github.com/l1nux-th1ngz/argp"
 license=('MIT')
-
 depends=('python')
 
-source=("$url/archive/v$pkgver.tar.gz")
+# Include the filename in the source array
+source=("$pkgname-$pkgver.tar.gz::${url}/archive/v$pkgver.tar.gz")
 
 build() {
     # No build actions are needed for a Python application
+    :
 }
 
 package() {
